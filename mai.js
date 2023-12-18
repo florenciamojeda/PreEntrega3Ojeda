fb93486eb90c79d789b89fbb5a428b5f7514ea75
Florencia Ojeda <florenciaojeda-@hotmail.com>
2023-12-18 20:32:22 -0300

    se agrega array

--- mai.js
+++ mai.js
@@ -5,29 +5,46 @@ let pregunta1;
 let pregunta2;
 let pregunta3;
 let pregunta4;
-let pregumta5;
+let pregunta5;
 let pregunta6;
 let contador = 0;
 let mensaje;
 let porcent = 0;
+let jugadores = 0;
+let participantes = 0;
+let indice = 0;
+let salir = 0;
+const resultado = [];
+const resultado2 = [];
 
 function error(p) {
     alert("Respuesta " + p + " mmmmmm tengo mis dudas");
     contador++;
 }
 
-usuario = prompt("Indicame tu Nombre:");
-alert("Hola " + usuario + " Vamos a hacerte un pequeño test para saber si podes manejar, estas listo ? Comencemos!!!!!!!");
-while (contador < 1) {
+jugadores = prompt("Cuantos participantes van a jugar?");
+
+if (isNaN(jugadores)) {
+    alert("Solo puede ingresar numeros");
+    jugadores = prompt("Cuantos participantes van a jugar?");
+}
+
+
+while (participantes < jugadores) {
+
+    usuario = prompt("Indicame tu Nombre:");
+    alert("Hola " + usuario + " Vamos a hacerte un pequeño test para saber si podes manejar, estas listo ? Comencemos!!!!!!!");
+
     pregunta1 = Number(prompt("cual es la edad minima para poder manejar: "));
 
-    if (pregunta1 >= 18) {
+    if (pregunta1 == 18) {
         alert("Respuesta " + pregunta1 + " es correcta");
     }
 
     else if (isNaN(pregunta1)) {
         alert("Error por no usar numeros, deberas voler a empesar");
         error(pregunta1);
+        salir = 1;
         break
     }
     else {
@@ -42,6 +59,7 @@ while (contador < 1) {
     else if (isNaN(pregunta2)) {
         alert("Error por no usar numeros, deberas voler a empesar");
         error(pregunta2);
+        salir = 1;
         break
     }
     else {
@@ -66,6 +84,7 @@ while (contador < 1) {
     else if (isNaN(pregunta4)) {
         alert("Error por no usar numeros, deberas voler a empesar");
         error(pregunta4);
+        salir = 1;
         break
     }
     else {
@@ -80,19 +99,39 @@ while (contador < 1) {
         error(pregunta5);
     }
 
-    break
+
+
+    if (contador > 0 && contador <= 2) { alert("Uh le pifiaste a " + contador + " preguntas, Declarado copiloto") }
+    else if (contador >= 3) { alert("Uh le pifiaste a " + contador + " preguntas, Te tomaste todo amigo!!Ahora tomate un uber") }
+    else { alert("Ningun error, Que grande Toretto, a casa") }
+
+
+    pregunta6 = prompt("Queres saber tu porcentage de borracho? decime que si");
+
+    if (pregunta6.toUpperCase() == "SI") {
+        porcent = contador * 100 / 5;
+
+        alert("Tu porcentage del alcohol es: " + porcent + "%");
+        contador = 0;
+
+    }
+    else {
+        contador = 0;
+    }
+
+    participantes++;
+
+    resultado.push(usuario);
+    resultado2.push(porcent);
+
 }
-if (contador > 0 && contador <= 2) { alert("Uh le pifiaste a " + contador + " preguntas, Declarado copiloto") }
-else if (contador >= 3) { alert("Uh le pifiaste a " + contador + " preguntas, Te tomaste todo amigo!!Ahora tomate un uber") }
-else { alert("Ningun error, Que grande Toretto, a casa") }
 
+if (salir = 0) {
+    for (let index = 0; index < jugadores; index++) {
 
-pregunta6 = prompt("Queres saber tu porcentage de borracho? decime que si");
+        alert(resultado[index] + " " + resultado2[index] + "%");
+    }
 
-if (pregunta6.toUpperCase() == "SI") {
-    porcent = contador * 100 / 5;
-
-    alert("Tu porcentage del alcohol es: " + porcent + "%");
+    indice = resultado2.indexOf(Math.min(...resultado2));
+    alert("El que tomo menos y contesto mas rapido es : " + resultado[indice] + " con el " + Math.min(...resultado2) + " % Felicitaciones Schumacher ");
 }
-
-else { }
